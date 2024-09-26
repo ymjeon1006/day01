dd8d0bcd145249c1f4de7cfa620de65f3aac2dca
ym <aa@aaa.com>
2024-09-26 13:09:04 +0900

    엔터 없는 출력

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -1,5 +1,17 @@
 package day01;
-
+//class 이름 규칙 : 첫 문자 대문자, 합성어 있으면 첫 문자 대문자
 public class ExClass01 {
-
+	//main은 프로그램의 시작점
+	public static void main(String[] args) {
+		//한줄 주석
+		/* 문단 주석 */
+		//syso + ctrl + space 자동완성
+		// sysout 은 괄호의 내용을 출력하는 용도
+		System.out.println( 100 );
+		System.out.println( "안녕하세요" );
+		System.out.print("1.안녕");
+		System.out.print("2.안녕");
+		System.out.print("3.안녕");
+		
+	}
 }
